52b482c231978e73c2be6aa3478075e35b16f4eb
Kleber Curvelo <62031937+KleberASGC@users.noreply.github.com>
2024-01-17 18:28:16 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const dbDependencies = responseBody.dependencies.database;
+  expect(dbDependencies.version).toBe("16.0");
+  expect(dbDependencies.max_connections).toBe(100);
+  expect(dbDependencies.opened_connections).toBe(1);
 });
